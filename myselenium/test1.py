9d4286b580df06c51fbb4ea3dfb5adc895a05a13
ibunao <idingran@163.com>
2018-10-24 23:11:17 +0800

    增加mac的driver

--- myselenium/test1.py
+++ myselenium/test1.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
 import time
+# win
+# driver = webdriver.Chrome(executable_path="./driver/chromedriver.exe")
+# mac
+driver = webdriver.Chrome(executable_path="./driver/mac/chromedriver")
+driver.get('https://www.douyu.com/5227917')
 
-driver = webdriver.Chrome(executable_path="./driver/chromedriver.exe")
-driver.get('https://www.douyu.com/1983057')
-
-time.sleep(180)
+time.sleep(120)
 print('睡眠结束')
-driver.find_element_by_xpath('//*[@id="js-player-asideMain"]/div/div[2]/div/div[2]/div[2]/textarea').send_keys('不孬')
+driver.find_element_by_xpath('//*[@id="js-player-asideMain"]/div/div[2]/div/div[2]/div[2]/textarea').send_keys('节日快乐！此信息由程序发出')
 driver.find_element_by_xpath('//*[@id="js-player-asideMain"]/div/div[2]/div/div[2]/div[2]/div').click()
 print('查看信息吧')
+time.sleep(60)
+driver.find_element_by_xpath('//*[@id="js-player-asideMain"]/div/div[2]/div/div[2]/div[2]/textarea').send_keys('哈哈哈哈哈哈！')
+driver.find_element_by_xpath('//*[@id="js-player-asideMain"]/div/div[2]/div/div[2]/div[2]/div').click()
+
 time.sleep(600)
 driver.quit()
